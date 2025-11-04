ab143a259a1c1ce108d3aff6f6760d4efd9fe7ae
sushimampfer <sushimampfer@duck.com>
2025-11-04 23:42:03 +0100

    plugin seems to work now

--- notes-frontend/src/app.rs
+++ notes-frontend/src/app.rs
@@ -5,15 +5,8 @@ use wasm_bindgen::prelude::*;
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
-
-#[wasm_bindgen(module = "tauri-plugin-mic-recorder-api")]
-extern "C" {
-    #[wasm_bindgen(js_name = startRecording)]
-    pub async fn start_recording();
-
-    #[wasm_bindgen(js_name = stopRecording)]
-    pub async fn stop_recording() -> JsValue;
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = "invoke")]
+    async fn invoke_argless(cmd: &str) -> JsValue;
 }
 
 #[component]
@@ -21,8 +14,8 @@ pub fn App() -> impl IntoView {
     let (path, set_path) = signal("".to_string());
 
     view! {
-        <button on:click=|_| spawn_local(start_recording())>Start</button>
-        <button on:click=move |_| spawn_local(async move {set_path.set(stop_recording().await.try_into().unwrap())})>Start</button>
+        <button on:click=|_| spawn_local(async {invoke_argless("plugin:mic-recorder|start_recording").await;})>Start</button>
+        <button on:click=move |_| spawn_local(async move {set_path.set(invoke_argless("plugin:mic-recorder|stop_recording").await.try_into().unwrap())})>Stop</button>
         <p>{path}</p>
     }
 }
