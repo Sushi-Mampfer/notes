19b6ac2198b10667f0a9e7b1f14c8fe0940bb44b
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2025-11-05 15:00:04 +0100

    working on moving some things to the backend

--- notes-frontend/src/app.rs
+++ notes-frontend/src/app.rs
@@ -5,17 +5,37 @@ use wasm_bindgen::prelude::*;
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = "invoke")]
     async fn invoke_argless(cmd: &str) -> JsValue;
+
+    fn listen() 
+}
+
+#[derive(Clone)]
+struct File {
+    pub id: u32,
+    pub path: String,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (path, set_path) = signal("".to_string());
+    let (files, add_file) = signal::<Vec<File>>(Vec::new());
+    let (count, set_count) = signal(0);
 
     view! {
         <button on:click=|_| spawn_local(async {invoke_argless("plugin:mic-recorder|start_recording").await;})>Start</button>
-        <button on:click=move |_| spawn_local(async move {set_path.set(invoke_argless("plugin:mic-recorder|stop_recording").await.try_into().unwrap())})>Stop</button>
-        <p>{path}</p>
+        <button on:click=move |_| spawn_local(async move {invoke("new_rec", invoke_argless("plugin:mic-recorder|stop_recording").await).await;})>Stop</button>
+        <ul>
+            <For
+                each=move || files.get()
+                key=|file| file.id
+                children=move |file| {
+                    view!{
+                        <li>{file.path}</li>
+                    }
+                }
+            />
+        </ul>
     }
 }
