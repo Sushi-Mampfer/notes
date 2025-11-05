5c2c97f499f5a41cdc22b79f0f5281e94a1c41cb
sushimampfer <sushimampfer@duck.com>
2025-11-05 22:32:48 +0100

    finally got backend<-> frontend communication working

--- notes-frontend/src-tauri/src/lib.rs
+++ notes-frontend/src-tauri/src/lib.rs
@@ -1,9 +1,35 @@
+use sqlx::{Pool, Sqlite};
+use tauri::{Emitter, Manager};
+
+#[derive(Debug)]
+struct AppData {
+    pub pool: Pool<Sqlite>,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+#[tokio::main]
+pub async fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            app.manage(AppData {
+                pool: Pool::connect_lazy(&dbg!(format!(
+                    "sqlite://{}\\db.sqlite",
+                    app.path()
+                        .app_data_dir()?
+                        .to_str()
+                        .ok_or("Path not valid")?
+                )))?,
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_mic_recorder::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![new_rec])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn new_rec(app_handle: tauri::AppHandle, path: String) {
+    app_handle.emit("test", path).unwrap();
 }
--- notes-frontend/src/app.rs
+++ notes-frontend/src/app.rs
@@ -1,4 +1,6 @@
 use leptos::{prelude::*, task::spawn_local};
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,23 +11,53 @@ extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = "invoke")]
     async fn invoke_argless(cmd: &str) -> JsValue;
 
-    fn listen() 
+    #[wasm_bindgen(js_namespace = ["console"])]
+    fn log(data: JsValue);
+
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"])]
+    async fn listen(event: &str, handler: &js_sys::Function) -> js_sys::Function;
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 struct File {
     pub id: u32,
+    pub path: String,
+}
+
+#[derive(Serialize)]
+struct NewRec {
     pub path: String,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
     let (files, add_file) = signal::<Vec<File>>(Vec::new());
-    let (count, set_count) = signal(0);
+
+    Effect::new(move || {
+        spawn_local(async move {
+            let cb: Closure<dyn Fn(JsValue)> = Closure::new(move |data: JsValue| {
+                add_file.update(|f| f.push(from_value(data).unwrap()));
+            });
+            listen("new_file", cb.as_ref().unchecked_ref()).await;
+            cb.forget();
+        });
+    });
+
+    Effect::new(|| {
+        spawn_local(async {
+            let cb: Closure<dyn Fn(JsValue)> = Closure::new(|data: JsValue| {
+                log(data);
+            });
+
+            listen("test", cb.as_ref().unchecked_ref()).await;
+
+            cb.forget()
+        })
+    });
 
     view! {
         <button on:click=|_| spawn_local(async {invoke_argless("plugin:mic-recorder|start_recording").await;})>Start</button>
-        <button on:click=move |_| spawn_local(async move {invoke("new_rec", invoke_argless("plugin:mic-recorder|stop_recording").await).await;})>Stop</button>
+        <button on:click=move |_| spawn_local(async move {invoke("new_rec", to_value(&NewRec {path: invoke_argless("plugin:mic-recorder|stop_recording").await.try_into().unwrap()}).unwrap()).await;})>Stop</button>
         <ul>
             <For
                 each=move || files.get()
