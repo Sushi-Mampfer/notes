677afaccb081bf77e3ded35b17b967add9b5b720
sushimampfer <sushimampfer@duck.com>
2025-11-06 22:43:07 +0100

    edit's working now, but not showing on the frontend

--- notes-frontend/src-tauri/src/lib.rs
+++ notes-frontend/src-tauri/src/lib.rs
@@ -1,18 +1,17 @@
 use serde::{Deserialize, Serialize};
-use sqlx::{query, sqlite::SqliteConnectOptions, Pool, Row, Sqlite};
+use sqlx::{query, query_as, sqlite::SqliteConnectOptions, FromRow, Pool, Row, Sqlite};
 use std::{fs, str::FromStr};
-use tauri::{async_runtime::spawn, Emitter, Manager};
+use tauri::{async_runtime::spawn, Emitter, Listener, Manager};
 
 struct AppData {
     pub pool: Pool<Sqlite>,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, FromRow)]
 struct Recording {
     pub id: u32,
     pub file: String,
     pub name: String,
-    pub note: Option<String>,
     pub uploaded: bool,
 }
 
@@ -29,14 +28,14 @@ pub async fn run() {
                     .create_if_missing(true),
             );
             app.manage(AppData { pool: pool.clone() });
+            let app_handle = app.handle().clone();
             spawn(async move {
                 query(
                     r#"
                     CREATE TABLE IF NOT EXISTS recordings (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         file TEXT NOT NULL,
-                        title TEXT NOT NULL,
-                        notes TEXT,
+                        name TEXT NOT NULL,
                         uploaded INTEGER
                     )
                 "#,
@@ -44,21 +43,31 @@ pub async fn run() {
                 .execute(&pool)
                 .await
                 .unwrap();
+                let rows: Vec<Recording> = query_as(r#"SELECT * FROM recordings ORDER BY id"#)
+                    .fetch_all(&pool)
+                    .await
+                    .unwrap();
+                let handle = app_handle.clone();
+                app_handle.listen_any("ready", move |_| {
+                    for r in &rows {
+                        handle.emit("file", r).unwrap();
+                    }
+                });
             });
             Ok(())
         })
         .plugin(tauri_plugin_mic_recorder::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![new_rec])
+        .invoke_handler(tauri::generate_handler![new, edit])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-async fn new_rec(app_handle: tauri::AppHandle, file: String, name: String) {
+async fn new(app_handle: tauri::AppHandle, file: String, name: String) {
     let row = query(
         r#"
-        INSERT INTO recordings (file, title, uploaded)
+        INSERT INTO recordings (file, name, uploaded)
         VALUES (?, ?, 0)
          RETURNING id
     "#,
@@ -76,9 +85,25 @@ async fn new_rec(app_handle: tauri::AppHandle, file: String, name: String) {
                 id,
                 file,
                 name,
-                note: None,
                 uploaded: false,
             },
         )
         .unwrap();
+}
+
+#[tauri::command]
+async fn edit(app_handle: tauri::AppHandle, id: u32, name: String) {
+    let rec: Recording = query_as(
+        r#"
+        UPDATE recordings SET name = ?
+        WHERE id = ?
+        RETURNING *
+    "#,
+    )
+    .bind(&name)
+    .bind(&id)
+    .fetch_one(&app_handle.state::<AppData>().pool)
+    .await
+    .unwrap();
+    app_handle.emit("file", rec).unwrap();
 }
--- notes-frontend/src/app.rs
+++ notes-frontend/src/app.rs
@@ -7,17 +7,29 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_namespace = ["console"])]
+    fn log(data: JsValue);
+
+    #[wasm_bindgen(js_namespace = ["window"])]
+    fn prompt(text: &str, placeholder: &str) -> Option<String>;
+
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = "invoke")]
     async fn invoke_argless(cmd: &str) -> JsValue;
 
-    #[wasm_bindgen(js_namespace = ["console"])]
-    fn log(data: JsValue);
-
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"])]
     async fn listen(event: &str, handler: &js_sys::Function) -> js_sys::Function;
+
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"], js_name = "emit")]
+    async fn emit_argless(event: &str);
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Edit {
+    id: u32,
+    name: String,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -25,7 +37,6 @@ struct RecordingId {
     pub id: u32,
     pub file: String,
     pub name: String,
-    pub note: Option<String>,
     pub uploaded: bool,
 }
 
@@ -39,7 +50,6 @@ struct Event<T> {
 struct Recording {
     pub file: String,
     pub name: String,
-    pub note: Option<String>,
     pub uploaded: bool,
 }
 
@@ -57,7 +67,6 @@ pub fn App() -> impl IntoView {
         spawn_local(async move {
             let cb: Closure<dyn Fn(JsValue)> = Closure::new(move |data: JsValue| {
                 add_file.update(|f| {
-                    log(data.clone());
                     let event: Event<RecordingId> = from_value(data).unwrap();
                     let rec = event.payload;
                     f.insert(
@@ -65,10 +74,10 @@ pub fn App() -> impl IntoView {
                         Recording {
                             file: rec.file,
                             name: rec.name,
-                            note: rec.note,
                             uploaded: rec.uploaded,
                         },
                     );
+                    log(to_value(&f).unwrap());
                 });
             });
             listen("file", cb.as_ref().unchecked_ref()).await;
@@ -76,28 +85,35 @@ pub fn App() -> impl IntoView {
         });
     });
 
-    Effect::new(|| {
-        spawn_local(async {
-            let cb: Closure<dyn Fn(JsValue)> = Closure::new(|data: JsValue| {
-                log(data);
-            });
-
-            listen("test", cb.as_ref().unchecked_ref()).await;
-
-            cb.forget()
-        })
-    });
+    Effect::new(|| spawn_local(emit_argless("ready")));
 
     view! {
         <button on:click=|_| spawn_local(async {invoke_argless("plugin:mic-recorder|start_recording").await;})>Start</button>
-        <button on:click=move |_| spawn_local(async move {invoke("new_rec", to_value(&NewRec {file: invoke_argless("plugin:mic-recorder|stop_recording").await.try_into().unwrap(), name: "test".to_string()}).unwrap()).await;})>Stop</button>
+        <button on:click=move |_| spawn_local(async move {
+            let name = match prompt("Name", "") {
+                Some(n) => n,
+                None => return,
+            };
+            invoke("new", to_value(&NewRec {file: invoke_argless("plugin:mic-recorder|stop_recording").await.try_into().unwrap(), name}).unwrap()).await;
+        })>Stop</button>
         <ul>
             <For
                 each=move || files.get()
                 key=|file| file.0
                 children=move |file| {
+                    let name = file.1.name.clone();
                     view!{
-                        <li>{file.1.file}</li>
+                        <li>
+                            <p>{file.1.name}</p>
+                            <button on:click=move |_| {
+                                let name = match prompt("Name", &name) {
+                                    Some(n) => n,
+                                    None => return,
+                                };
+                                spawn_local(async move {invoke("edit", to_value(&Edit{ id: file.0, name }).unwrap()).await;});
+                            }><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c0c0c0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg></button>
+                            <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c0c0c0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg></button>
+                        </li>
                     }
                 }
             />
